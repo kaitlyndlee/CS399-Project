2bf7593465a8dc70df6c43c1d6b437ca079c9ce1
Kaitlyn <kdl222@nau.edu>
2019-11-23 20:28:32 -0700

    Changed image based off of what the user inputs.

--- rpg/app/src/main/java/com/example/rpg/main_game_activity.kt
+++ rpg/app/src/main/java/com/example/rpg/main_game_activity.kt
@@ -2,11 +2,43 @@ package com.example.rpg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.main_game_screen.*
 
 class main_game_activity : AppCompatActivity() {
+    lateinit var  enemy1: Architecture
+    lateinit var  enemy2: Architecture
+    lateinit var  player: Architecture
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_game_screen)
+        val userInput = getIntent().getStringExtra("selectedArch");
+        createPlayer(userInput)
+
+        createEnemies()
     }
+
+    fun createEnemies() {
+        enemy1 = Tank(getString(R.string.enemy1Name))
+        enemy1Health.text = enemy1.health.toString()
+        enemy2 = Mage(getString(R.string.enemy1Name))
+        enemy2Health.text = enemy2.health.toString()
+    }
+
+    fun createPlayer(archType: String) {
+        if(archType == "mage") {
+            val player:ImageView = findViewById(R.id.player)
+            player.setImageResource(R.drawable.mage)
+        }
+        else if(archType == "tank") {
+            val player:ImageView = findViewById(R.id.player)
+            player.setImageResource(R.drawable.tank)
+        }
+        else {
+            val player:ImageView = findViewById(R.id.player)
+            player.setImageResource(R.drawable.healer)
+        }
+    }
+
 }
--- rpg/app/src/main/java/com/example/rpg/new_game_activity.kt
+++ rpg/app/src/main/java/com/example/rpg/new_game_activity.kt
@@ -16,16 +16,19 @@ class new_game_activity : AppCompatActivity() {
 
     fun selectMage(v: View) {
         val myIntent = Intent(baseContext, main_game_activity::class.java)
+        myIntent.putExtra("selectedArch", "mage")
         startActivity(myIntent)
     }
 
     fun selectTank(v: View) {
         val myIntent = Intent(baseContext, main_game_activity::class.java)
+        myIntent.putExtra("selectedArch", "tank")
         startActivity(myIntent)
     }
 
     fun selectHealer(v: View) {
         val myIntent = Intent(baseContext, main_game_activity::class.java)
+        myIntent.putExtra("selectedArch", "healer")
         startActivity(myIntent)
     }
 
